c94ce94ac5e7032e815b284a689c8ee0e131687a
Alessandro Perez <alessandrogperez@gmail.com>
2024-08-26 20:34:58 +0200

    add 2.add-two-numbers

--- 2.add-two-numbers/main.cpp
+++ 2.add-two-numbers/main.cpp
@@ -1,81 +1,84 @@
 ﻿#include <iostream>
-#include <cmath>
 
 // Definition for singly-linked list.
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-void printList(ListNode* head);
-int listToInt(ListNode* head);
-
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+
+        ListNode *dummyHead = new ListNode(0);
+        ListNode *p = l1, *q = l2, *current = dummyHead;
+        int carry = 0;
+
+        while (p != nullptr || q != nullptr)
+        {
+            int x = (p != nullptr) ? p->val : 0;
+            int y = (q != nullptr) ? q->val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (p != nullptr)
+                p = p->next;
+            if (q != nullptr)
+                q = q->next;
+        }
+
+        if (carry > 0)
+        {
+            current->next = new ListNode(carry);
+        }
+
+        return dummyHead->next;
+    }
+
+    void printList(ListNode *head)
+    {
+        ListNode *current = head;
+
+        while (current != nullptr)
+        {
+            std::cout << current->val << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    int listToInt(ListNode *head)
+    {
+        ListNode *current = head;
+        int finalNumber = 0;
+
+        while (current != nullptr)
+        {
+            finalNumber = finalNumber * 10 + current->val;
+            current = current->next;
+        }
+
+        return finalNumber;
     }
 };
 
 int main()
 {
-    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    ListNode *l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode *l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
-    printList(l1);
-    listToInt(l1);
+    Solution solution;
 
-    std::cout << listToInt(l1) + listToInt(l2);
+    ListNode *lFinal = solution.addTwoNumbers(l1, l2);
+    solution.printList(lFinal);
 
     return 0;
-}
-
-void printList(ListNode* head)
-{
-    ListNode* current = head;
-
-    while( current != nullptr)
-    {
-        std::cout << current->val << " ";
-        current = current->next;
-    }
-    std::cout << std::endl;
-}
-
-int listToInt(ListNode* head)
-{
-    ListNode* current = head;
-    int finalNumber = 0;
-    int listSize = 0;
-
-    // Get listSize
-    while( current != nullptr)
-    {
-        ++ listSize;
-        current = current->next;
-    }
-    //std::cout << listSize;
-
-    current = head;
-    int listPos = listSize;
-    // Calculate finalNumber
-    while( current != nullptr)
-    {
-        if (listSize - listPos == 0)
-        {
-            finalNumber = current->val;
-            current = current->next;
-            --listPos;
-        }
-            finalNumber += current->val * pow(10, listSize - listPos);
-            current = current->next;
-            --listPos;
-    }
-
-    //std::cout << finalNumber;
-
-    return finalNumber;
 }
